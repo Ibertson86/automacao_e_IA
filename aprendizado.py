f45fc1fd50833878010ec9d86479328a35e96c1d
Ibertson Medeiros <165746742+Ibertson86@users.noreply.github.com>
2025-02-24 21:06:32 -0300

    Add files via upload

---
+++ aprendizado.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, r2_score
+
+def visualizar_dados(df, feature_names):
+    sns.set_theme(style="whitegrid", palette='muted')
+    plt.rcParams["figure.figsize"] = (10,6)
+    print(df.head())
+    df.hist(bins=30, figsize=(15,10))
+    plt.suptitle("Histograma", fontsize=16)
+    plt.show()
+
+print("BEM-VINDO AO AKINATOR DE MÓVEIS v0.1")
+
+#1° passo: Carregar os dados
+
+dados = fetch_california_housing()
+X,y = dados.data, dados.target
+feature_names = dados.feature_names
+df = pd.DataFrame(X, columns = feature_names)
+df['MedHouseVal'] = y
+
+#2° passo: Opção a partir do usuário - Visualização
+visualizar = input("Deseja visualizar os dados antes de treinar o modelo?")
+if visualizar == "s":
+    visualizar_dados(df, feature_names)
+
+#3° passo: Dividindo dados para treino e teste
+print("Dividindo dados para treino e teste")
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
+print("Processo concluído")
+
+#4° passo: Normalizar os dados
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+print("Normalização concluída")
+
+#5° passo: Treinamento do modelo de Regressão Linear
+model = LinearRegression()
+model.fit(X_train, y_train)
+print("Treinamento concluído")
+
+#6° passo: Avaliação do modelo - Testes
+y_pred = model.predict(X_test)
+mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print(f"Erro Médio Absoluto: {mae:.2f}")
+print(f"Coeficiente de Determinação: {r2:.2f}")
+
+
+
